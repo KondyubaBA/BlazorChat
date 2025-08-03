cb9d3e3ceb65c7b7a3832db9f08bb953266ae02c
Bogdan <sbis86@mail.ru>
2025-08-03 22:26:19 +0500

    Init
    Init

---
+++ BlazorChat/BlazorChat/Program.cs
@@ -0,0 +1,48 @@
+using BlazorChat.Components;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSignalR();
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.MapHub<ChatHub>("/chatHub");
+            app.Run();
+        }
+    }
+}
+
+// Ваш хаб
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        // Рассылаем всем подключённым клиентам
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
